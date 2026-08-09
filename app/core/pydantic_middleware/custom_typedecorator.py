6791a0b14b97464a05b1fe0e69963c51e88a3f4b
Abhishek <hp7320797@gmail.com>
2026-08-09 12:05:09 +0545

    Refactor PydanticType: reorganize methods and improve type hints for clarity

--- app/core/pydantic_middleware/custom_typedecorator.py
+++ app/core/pydantic_middleware/custom_typedecorator.py
@@ -1,26 +1,36 @@
+from typing import Any
+
+from pydantic import BaseModel
 from sqlalchemy import TypeDecorator
 from sqlalchemy.dialects.postgresql import JSONB
-from pydantic import BaseModel
-from typing import Any
+
 
 class PydanticType(TypeDecorator):
     """
     Automatically converts Pydantic models to JSONB and back.
     """
+
     impl = JSONB
     cache_ok = True
 
     def __init__(self, pydantic_model: type[BaseModel]):
+        super().__init__()
         self.pydantic_model = pydantic_model
 
-    def process_bind_param(self, value: Any, dialect) -> dict:
+    def load_dialect_impl(self, dialect):
+        return dialect.type_descriptor(JSONB())
+
+    def python_type(self):
+        return self.pydantic_model
+
+    def process_bind_param(self, value: Any, dialect) -> dict | None:
         if value is not None:
             if isinstance(value, self.pydantic_model):
                 return value.model_dump()
             return value
         return None
 
-    def process_result_value(self, value: Any, dialect) -> BaseModel:
+    def process_result_value(self, value: Any, dialect) -> BaseModel | None:
         if value is not None:
             return self.pydantic_model.model_validate(value)
         return None
